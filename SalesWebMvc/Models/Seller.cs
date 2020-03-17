cb638c1e168381083dc87ade4d5c0b36ba3c1db0
Giovanne Rolo <giovannerolo@outlook.com>
2020-03-17 19:43:08 -0300

    SellerService and basic FindAll

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -10,7 +10,7 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public DateTime BirthDay { get; set; }
+        public DateTime BirthDay{ get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
